29072b618e7596ad9a28e6f36b1a7772cb6bb4c0
Badgerenko <badgerenko@gmail.com>
2025-12-14 02:54:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
 import os
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import Message
+from aiogram.filters import Command
+from aiogram import F
+import asyncio
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher()
 
-@dp.message_handler()
-async def echo(message: types.Message):
+@dp.message(F.text)
+async def echo(message: Message):
     await message.answer("✅ Бот працює")
 
+async def main():
+    dp.startup.register(lambda _: print("Bot started!"))
+    await dp.start_polling(bot)
+
 if __name__ == "__main__":
-    executor.start_polling(dp)
+    asyncio.run(main())
